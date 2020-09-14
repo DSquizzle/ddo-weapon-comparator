c288513e064a632648f261d4ac4771fd31b47a2b
Danish Roshan <droshan@aeonglobalhealth.com>
2020-09-14 14:35:35 -0400

    Replaced "Save" button with "Delete" button. Implemented autosave feature. Plots now update automatically as input parameters for their respective weapons are changed. The legend has a tendency to break when plots are automatically updated.

--- gui.py
+++ gui.py
@@ -24,26 +24,27 @@ class MainWindow(QMainWindow):
 
         self.weaponInfoFields = []
         self.weapons = {}
+        self.plottedWeapons = {}
 
         self.weaponSelector = QComboBox()
         self.weaponSelector.addItem("New Weapon")
         self.weaponSelector.currentTextChanged.connect(self.loadWeapon)
-        self.saveButton = QPushButton("Save")
-        self.saveButton.setDisabled(True)
-        self.saveButton.clicked.connect(self.onSaveButtonClicked)
+        self.deleteButton = QPushButton("Delete")
+        #self.deleteButton.setDisabled(True)
+        self.deleteButton.clicked.connect(self.onDeleteButtonClicked)
         self.newButton = QPushButton("New")
         self.newButton.clicked.connect(self.onNewButtonClicked)
         weaponSelectorLayout = QHBoxLayout()
         weaponSelectorLayout.addWidget(QLabel("Select a weapon to edit"), 4)
         weaponSelectorLayout.addWidget(self.weaponSelector, 10)
-        weaponSelectorLayout.addWidget(self.saveButton, 2)
+        weaponSelectorLayout.addWidget(self.deleteButton, 2)
         weaponSelectorLayout.addWidget(self.newButton, 2)
         weaponSelectorLayout.setContentsMargins(20, 0, 0, 0)
 
         self.fields = [["Weapon Name", "e.g. Insanity", ".*"],
                        ["Weapon Dice Multiplier", "e.g. 1", "\\d*(\\.\\d+)?"],
                        ["Base Damage Dice", "e.g. 2d6", "\\d*d\\d+"],
-                       ["Critical Profile", "e.g. 19-20x2", "[01]?\\d-20x\\d+"],
+                       ["Critical Profile", "e.g. 19-20x2", "[12]?\\d-20x\\d+"],
                        ["On-Hit Damage", "e.g. 3d6", "\\d*d\\d+"]]
 
         grid = QGridLayout()
@@ -58,8 +59,11 @@ class MainWindow(QMainWindow):
         grid.setContentsMargins(20, 40, 100, 20)
 
         self.addPlotButton = QPushButton("Add Plot")
+        self.addPlotButton.setDisabled(True)
         self.addPlotButton.clicked.connect(self.plotWeapon)
         self.removePlotButton = QPushButton("Remove Plot")
+        self.removePlotButton.setDisabled(True)
+        self.removePlotButton.clicked.connect(self.removePlot)
         plotButtonsLayout = QHBoxLayout()
         plotButtonsLayout.addWidget(self.addPlotButton)
         plotButtonsLayout.addWidget(self.removePlotButton)
@@ -82,12 +86,41 @@ class MainWindow(QMainWindow):
         widget.setLayout(columns)
         self.setCentralWidget(widget)
 
+    def onInputFieldChanged(self):
+        #self.saveButton.setEnabled(self.inputsAreAcceptable())
+        self.addPlotButton.setEnabled(self.plotIsAddable())
+        self.removePlotButton.setEnabled(self.plotIsRemovable())
+
+        if self.inputsAreAcceptable():
+            self.saveWeapon()
+            if graph := self.plottedWeapons.get(self.weaponSelector.currentText(), None):
+                if not graph.get_figure():
+                    self.chart.axes.add_artist(graph)
+                graph.set_ydata([self.weapons[self.weaponSelector.currentText()].averageDamage(deadly) for deadly in range(50)])
+                self.chart.axes.legend()
+                self.chart.draw()
+        else:
+            if graph := self.plottedWeapons.get(self.weaponSelector.currentText(), None):
+                self.fig = graph.get_figure() # Need a better way to save self.fig
+                if graph.get_figure():
+                    graph.remove()
+                    self.chart.axes.legend()
+                    self.chart.draw()
+
     def onNewButtonClicked(self):
         self.weaponSelector.setCurrentText("New Weapon")
         for field in self.weaponInfoFields:
             field.clear()
 
-    def onSaveButtonClicked(self):
+    def onDeleteButtonClicked(self):
+        if self.weaponSelector.currentText() != "New Weapon":
+            del(self.weapons[self.weaponSelector.currentText()])
+            self.removePlot()
+            #del(self.plottedWeapons[self.weaponSelector.currentText()])
+            # Above commented line is not necessary thanks to Python bindings
+            self.weaponSelector.removeItem(self.weaponSelector.currentIndex())
+
+    def saveWeapon(self):
         weaponInfo = [self.weaponInfoFields[i].text() for i in range(5)]
         wName = weaponInfo[0]
         self.weapons[weaponInfo[0]] = Weapon(*weaponInfo)
@@ -99,13 +132,6 @@ class MainWindow(QMainWindow):
             self.weaponSelector.removeItem(self.weaponSelector.currentIndex())
         self.weaponSelector.setCurrentText(wName)
 
-    def onInputFieldChanged(self):
-        for field in self.weaponInfoFields:
-            if not (field.hasAcceptableInput() and field.text()):
-                return self.saveButton.setDisabled(True)
-
-        return self.saveButton.setEnabled(True)
-
     def loadWeapon(self, name):
         if name == "New Weapon":
             return self.onNewButtonClicked()
@@ -114,12 +140,39 @@ class MainWindow(QMainWindow):
             self.weaponInfoFields[field].setText(str(value))
 
     def plotWeapon(self):
-        self.onSaveButtonClicked()
+        self.saveWeapon()
         weapon = self.weapons[self.weaponSelector.currentText()]
-        data = [range(100), [weapon.averageDamage(deadly) for deadly in range(100)]]
-        self.chart.axes.plot(*data, label=weapon.name)
+        data = [range(50), [weapon.averageDamage(deadly) for deadly in range(50)]]
+        self.plottedWeapons[weapon.name] = self.chart.axes.plot(*data, label=weapon.name)[0]
         self.chart.axes.legend()
         self.chart.draw()
+        self.removePlotButton.setEnabled(True)
+        self.addPlotButton.setDisabled(True)
+
+    def removePlot(self):
+        if plot := self.plottedWeapons.get(self.weaponSelector.currentText(), None):
+            plot.remove()
+            del(self.plottedWeapons[self.weaponSelector.currentText()])
+            self.chart.axes.legend()
+            self.chart.draw()
+            self.removePlotButton.setDisabled(True)
+            self.addPlotButton.setEnabled(self.plotIsAddable())
+
+    def plotIsRemovable(self):
+        return bool(self.plottedWeapons.get(self.weaponSelector.currentText(), None))
+
+    def plotIsAddable(self):
+        if self.inputsAreAcceptable():
+            return False if self.plotIsRemovable() else True
+
+        return False
+
+    def inputsAreAcceptable(self):
+        for field in self.weaponInfoFields:
+            if not (field.hasAcceptableInput() and field.text()):
+                return False
+
+        return True
 
 
 app = QApplication([])
