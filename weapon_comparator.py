8d6f43ca94ab68436ebfce34665a5f4f1c8e8b21
Danish Roshan <droshan@aeonglobalhealth.com>
2020-09-18 14:13:55 -0400

    Created "DamageExpression" class to handle arbitrary sums consisting of DamageDice strings and numerical strings

--- weapon_comparator.py
+++ weapon_comparator.py
@@ -7,6 +7,9 @@ matplotlib.use('Qt5Agg')
 TEXT_GREEN = '\033[32m'
 ENDC = '\033[m'
 
+class float(float):
+    def averageDamage(self):
+        return self
 
 class Weapon:
     def __init__(self, name, weaponDiceMultiplier,
@@ -20,6 +23,20 @@ class Weapon:
 
     def averageDamage(self, deadly):
         return ((self.weaponDiceMultiplier * self.damageDice.averageDamage() + deadly) * (self.critProfile.effectiveHits() / 19) + getattr(self.onHit, 'averageDamage', lambda: self.onHit)())
+
+
+class DamageExpression:
+    def __init__(self, string):
+        self.summands = []
+        summands = string.split('+')
+        for summand in summands:
+            self.summands.append(DamageDice(summand) if 'd' in summand else float(summand))
+
+    def __str__(self):
+        return ' + '.join([str(summand) for summand in self.summands])
+
+    def averageDamage(self):
+        return sum(summand.averageDamage() for summand in self.summands)
 
 
 class DamageDice:
